4449e5b1018a1eea6acf78efd8a34a14cada96cc
sosazi9847 <sosazi9847@163.com>
2024-07-12 13:28:29 +0800

    Update #CAIJI_ppt.py

--- ZUZU-PY/#CAIJI_ppt.py
+++ ZUZU-PY/#CAIJI_ppt.py
@@ -8,44 +8,43 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-#enter
+
 files = os.listdir('rtp')
 
 files_name = []
 
-#enter
+
 for file in files:
     name, extension = os.path.splitext(file)
     files_name.append(name)
 
-#enter
+
 provinces_isps = [name for name in files_name if name.count('_') == 1]
 
-#enter
-print(f"enter：{provinces_isps}enter") 
+
+print(f"本次查询：{provinces_isps}的组播节目") 
 
 keywords = []
 
 for province_isp in provinces_isps:
-    #enter
+
     try:
         with open(f'rtp/{province_isp}.txt', 'r', encoding='utf-8') as file:
             lines = file.readlines()
             lines = [line.strip() for line in lines if line.strip()]
-        #enter
+
         if lines:
             first_line = lines[0]
             if "rtp://" in first_line:
                 mcast = first_line.split("rtp://")[1].split(" ")[0]
                 keywords.append(province_isp + "_" + mcast)
     except FileNotFoundError:
-    #enter
-        print(f"enter '{province_isp}.txt' enter.")
+
+        print(f"文件 '{province_isp}.txt' 不存在. 跳过此文件.")
 
 for keyword in keywords:
     province, isp, mcast = keyword.split("_")
-    #enter
-    #enter
+
     if province == "北京" and isp == "联通":
         isp_en = "cucc"
         org = "China Unicom Beijing Province Network"
@@ -69,53 +68,40 @@ for keyword in keywords:
         try:
             search_url = 'https://fofa.info/result?qbase64='
             search_txt = f'\"udpxy\" && country=\"CN\" && region=\"{province}\" && org=\"{org}\"'
-                #enter
             bytes_string = search_txt.encode('utf-8')
-                #enter
             search_txt = base64.b64encode(bytes_string).decode('utf-8')
             search_url += search_txt
-            print(f"{current_time} enter : {province}{isp} ，enter : {search_url}")
+            print(f"{current_time} 查询运营商 : {province}{isp} ，查询网址 : {search_url}")
             response = requests.get(search_url, timeout=5)
-            #enter
             response.raise_for_status()
-            #enter
             html_content = response.text
-            #enter
+
             html_soup = BeautifulSoup(html_content, "html.parser")
-            # print(f"{current_time} html_content:{html_content}")
-            #enter
-            #enter
+
             pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
             urls_all = re.findall(pattern, html_content)
-            #enter
+
             result_urls = set(urls_all)
             print(f"{current_time} result_urls:{result_urls}")
 
             valid_ips = []
 
-            #enter
             for url in result_urls:
                 video_url = url + "/rtp/" + mcast
 
-                #enter
                 cap = cv2.VideoCapture(video_url)
 
-                #enter
                 if not cap.isOpened():
-                    print(f"{current_time} {video_url} enter")
+                    print(f"{current_time} {video_url} 无效")
                 else:
-                    #enter
                     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    print(f"{current_time} {video_url} enter{width}x{height}")
-                    #enter
+                    print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
                     if width > 0 and height > 0:
                         valid_ips.append(url)
-                    #enter
                     cap.release()
                     
             if valid_ips:
-                #enter
                 rtp_filename = f'rtp/{province}_{isp}.txt'
                 with open(rtp_filename, 'r', encoding='utf-8') as file:
                     data = file.read()
@@ -125,14 +111,15 @@ for keyword in keywords:
                         new_data = data.replace("rtp://", f"{url}/rtp/")
                         new_file.write(new_data)
 
-                print(f'enter，enter{txt_filename}')
+                print(f'已生成播放列表，保存至{txt_filename}')
 
         except (requests.Timeout, requests.RequestException) as e:
             timeout_cnt += 1
-            print(f"{current_time} [{province}]enter：{timeout_cnt}")
+            print(f"{current_time} [{province}]搜索请求发生超时，异常次数：{timeout_cnt}")
             if timeout_cnt <= 3:
-                    #enter
+
                 continue
             else:
-                print(f"{current_time} enter[]，enter：{timeout_cnt} enter，enter")
-print('enter！')
+                print(f"{current_time} 搜索IPTV频道源[]，超时次数过多：{timeout_cnt} 次，停止处理")
+print('over')
+
