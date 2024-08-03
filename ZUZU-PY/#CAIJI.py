30c338bc06ab6da81a9d1c47d0bf2d9d57e03a8a
sosazi9847 <sosazi9847@163.com>
2024-08-03 08:54:20 +0800

    Update #CAIJI.py

--- ZUZU-PY/#CAIJI.py
+++ ZUZU-PY/#CAIJI.py
@@ -1,21 +1,17 @@
 import os
 import requests
-import re
-import base64
-import cv2
-from urllib.parse import urlparse
 
-# 获取rtp目录下的文件名
+
+
 files = os.listdir('rtp')
 
 files_name = []
 
-# 去除后缀名并保存至provinces_isps
 for file in files:
     name, extension = os.path.splitext(file)
     files_name.append(name)
 
-#忽略不符合要求的文件名
+
 provinces_isps = [name for name in files_name if name.count('_') == 1]
 
 # 打印结果
@@ -41,21 +37,6 @@ for province_isp in provinces_isps:
 
 for keyword in keywords:
     province, isp, mcast = keyword.split("_")
-    #将省份转成英文小写
-    # 根据不同的 isp 设置不同的 org 值
-    if province == "北京" and isp == "联通":
-        isp_en = "cucc"
-        org = "China Unicom Beijing Province Network"
-    elif isp == "联通":
-        isp_en = "cucc"
-        org = "CHINA UNICOM China169 Backbone"
-    elif isp == "电信":
-        org = "Chinanet"
-        isp_en = "ctcc"
-    elif isp == "移动":
-        org == "China Mobile communications corporation"
-        isp_en = "cmcc"
-        
 #    else:
 #        org = ""
 
@@ -79,16 +60,7 @@ for keyword in keywords:
             html_content = response.text
             # 使用BeautifulSoup解析网页内容
             html_soup = BeautifulSoup(html_content, "html.parser")
-            # print(f"{current_time} html_content:{html_content}")
-            # 查找所有符合指定格式的网址
-            # 设置匹配的格式，如http://8.8.8.8:8888
-            pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
-            urls_all = re.findall(pattern, html_content)
-            # 去重得到唯一的URL列表
-            result_urls = set(urls_all)
-            print(f"{current_time} result_urls:{result_urls}")
 
-            valid_ips = []
 
             # 遍历所有视频链接
             for url in result_urls:
@@ -101,26 +73,12 @@ for keyword in keywords:
                 if not cap.isOpened():
                     print(f"{current_time} {video_url} 无效")
                 else:
-                    # 读取视频的宽度和高度
-                    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
-                    # 检查分辨率是否大于0
-                    if width > 0 and height > 0:
-                        valid_ips.append(url)
-                    # 关闭视频流
                     cap.release()
                     
             if valid_ips:
                 #生成节目列表 省份运营商.txt
                 rtp_filename = f'rtp/{province}_{isp}.txt'
-                with open(rtp_filename, 'r', encoding='utf-8') as file:
-                    data = file.read()
-                txt_filename = f'{province}{isp}.txt'
-                with open(txt_filename, 'w') as new_file:
-                    for url in valid_ips:
-                        new_data = data.replace("rtp://", f"{url}/rtp/")
-                        new_file.write(new_data)
+
 
                 print(f'已生成播放列表，保存至{txt_filename}')
 
